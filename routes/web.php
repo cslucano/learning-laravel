e7e070ad898218b596d9689fb6cfab4e399ed65d
Carlos Salvatierra <cslucano@gmail.com>
2023-01-13 11:24:12 -0500

    episodio 10

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,14 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if(! file_exists($path)) {
+        if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
         // abort(404);
         // dd('files does not exists');
     }
 
-    $post = file_get_contents($path); 
-
+    $post = cache()->remember('posts.{$slug}', now()->addMinutes(20), fn() => file_get_contents($path));     
+    
     return view('post', [
         'post' => $post,
     ]);
