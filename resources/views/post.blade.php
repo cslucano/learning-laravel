d570bafd633a92ad4b08030f7e0505f8483dd146
Carlos Salvatierra <cslucano@gmail.com>
2023-01-13 10:40:43 -0500

    episodio 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,8 +7,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
+        <?= $post; ?>
      </article>
      <a href="/">Go back</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,15 +7,15 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
      </article>
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
     </article>
     <article>
-        <h1><a href="/post">My Third Post</a> </h1>
+        <h1><a href="/posts/my-third-post">My Third Post</a> </h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
     </article>
 </body>
