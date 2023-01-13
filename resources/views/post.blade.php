b7c4767a837b49fb4be36e8646397cadcf105d68
Carlos Salvatierra <cslucano@gmail.com>
2023-01-13 01:09:35 -0500

    episode7

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css">
+<script src="/app.js"></script>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
+     </article>
+     <a href="/">Go back</a>
+</body>
+
+   
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css">
+<script src="/app.js"></script>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
+     </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a> </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
+    </article>
+</body>
+
+   
