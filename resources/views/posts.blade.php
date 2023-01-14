840ffa8d4d2ddbf2cb86c113b613a9185ddf19c1
Carlos Salvatierra <cslucano@gmail.com>
2023-01-14 01:51:58 -0500

    episode 11

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,18 +6,11 @@
 <script src="/app.js"></script>
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
-     </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a> </h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt iste explicabo esse et, sint perferendis ullam blanditiis voluptatum repudiandae repellendus obcaecati accusamus eaque, rem maxime rerum repellat, amet eveniet adipisci.</p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
 
    
