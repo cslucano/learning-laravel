17d559abef923d9150d3526922237fe91333f631
Carlos Salvatierra <cslucano@gmail.com>
2023-01-17 17:21:03 -0500

    episode 16

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\File;
 
@@ -44,4 +46,16 @@ class Post
     public static function find($slug) {
         return static::all()->firstWhere('slug', $slug);
     }
+
+    public static function findOrFail($slug) {
+        $post = Post::find( $slug);
+
+        if (!$post)
+        {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
+    }
+
 }
