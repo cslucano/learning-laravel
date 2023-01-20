df6f4079670ea2cf7737d9fa17d86903aacd2a68
Carlos Salvatierra <cslucano@gmail.com>
2023-01-20 01:18:24 -0500

    episode 27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Category::truncate();
+        Post::truncate();
+        User::truncate();
+        
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        $hobby =Category::create([
+            'name' => 'Hobby',
+            'slug' => 'hobby',
+        ]);
+
+          
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My First Post',
+            'slug' => 'my-first-post',
+            'excerpt' => '<p>Lorem ipsum.</p>',
+            'body' => "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ut rem qui, id error delectus inventore dolorem veritatis quia voluptas. Dolorem dolorum voluptate adipisci in nisi veniam maxime voluptatibus dolor?</p>"
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum.</p>',
+            'body' => "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ut rem qui, id error delectus inventore dolorem veritatis quia voluptas. Dolorem dolorum voluptate adipisci in nisi veniam maxime voluptatibus dolor?</p>"
+        ]);
+
     }
 }
+// 
