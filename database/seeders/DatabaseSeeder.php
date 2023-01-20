f1f931f25797b7f92e86a9a440ccb9138ad897b1
Carlos Salvatierra <cslucano@gmail.com>
2023-01-20 02:13:00 -0500

    episode 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,47 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Category::truncate();
-        Post::truncate();
-        User::truncate();
-        
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
+        $user = User::factory()->create([
+            'name' => 'Carlos Salvatierra'
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        $hobby =Category::create([
-            'name' => 'Hobby',
-            'slug' => 'hobby',
-        ]);
-
-          
-        Post::create([
+        Post::factory(5)->create([
             'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My First Post',
-            'slug' => 'my-first-post',
-            'excerpt' => '<p>Lorem ipsum.</p>',
-            'body' => "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ut rem qui, id error delectus inventore dolorem veritatis quia voluptas. Dolorem dolorum voluptate adipisci in nisi veniam maxime voluptatibus dolor?</p>"
         ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum.</p>',
-            'body' => "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ut rem qui, id error delectus inventore dolorem veritatis quia voluptas. Dolorem dolorum voluptate adipisci in nisi veniam maxime voluptatibus dolor?</p>"
-        ]);
-
     }
 }
 // 
