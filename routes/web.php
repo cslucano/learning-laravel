e764b949f280a0e1fd994d474283ca49dbbeec00
Carlos Salvatierra <cslucano@gmail.com>
2023-01-23 11:44:19 -0500

    episode 41

--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,7 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']) ; 
-
-Route::get('authors/{author:username}', function(User $author){
-    return view('posts',[
-        'posts' => $author->posts,
-    ]);
-});
