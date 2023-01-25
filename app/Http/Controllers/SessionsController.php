b28186bad7ffd1169bf2d30a866ae3f45482b30b
Carlos Salvatierra <cslucano@gmail.com>
2023-01-24 22:16:12 -0500

    episode 50

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,10 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            
+            return redirect('/')->with('success','Bienvenido nuevamente!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Las credenciales proporcionadas no han podido ser verificadas.',
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
