8bc4aafa1d04ca207a1698739c7fa01e368e0e20
Carlos Salvatierra <cslucano@gmail.com>
2023-01-26 11:05:26 -0500

    episode 52

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
