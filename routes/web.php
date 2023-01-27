660a87dddc5454113ea2decb1deabd10cd3a9fe4
Carlos Salvatierra <cslucano@gmail.com>
2023-01-27 18:41:50 -0500

    episode 58

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us17'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('4a5487ee4a',[
+        'email_address' => 'cslucano@hotmail.com',
+        'status' => 'subscribed',
+    ]);
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
