34df27d392ae22cd04e63b0e42631fc7e1472c52
Carlos Salvatierra <cslucano@gmail.com>
2023-01-31 23:16:05 -0500

    episode 66

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,7 +1,7 @@
 @props(['name'])
 <x-form.field>
     <x-form.label name="{{ $name }}" />
-    <textarea class="w-full text-sm focus:outline-none focus:ring" name="{{ $name }}" id="{{ $name }}"
+    <textarea class="border border-gray-200 p-2 w-full rounded" name="{{ $name }}" id="{{ $name }}"
         rows="5" placeholder="" required>{{ old($name) }}</textarea>
     <x-form.error name="{{ $name }}" />
 </x-form.field>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,31 +1,26 @@
 <x-layout>
-    <section class="py-8 max-w-md mx-auto">
-        <h1 class="text-lg font-bold mb-4">
-            Publicar Nuevo Post
-        </h1>
-        <x-panel>
-            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
-                @csrf
+    <x-setting heading="Publicar Nuevo Post">
+        <form method="POST" action="/admin/posts" enctype="multipart/form-data">
+            @csrf
 
-                <x-form.input name="title" />
-                <x-form.input name="slug" />
-                <x-form.input name="thumbnail" type="file" />
-                <x-form.textarea name="excerpt" />
-                <x-form.textarea name="body" />
+            <x-form.input name="title" />
+            <x-form.input name="slug" />
+            <x-form.input name="thumbnail" type="file" />
+            <x-form.textarea name="excerpt" />
+            <x-form.textarea name="body" />
 
-                <x-form.field>
-                    <x-form.label name="category" />
-                    <select name="category_id" id="category">
-                        @foreach (\App\Models\Category::all() as $category)
-                            <option value="{{ $category->id }}"
-                                {{ old('category_id') == $category->id ? 'selected' : '' }}>
-                                {{ ucwords($category->name) }}</option>
-                        @endforeach
-                    </select>
-                    <x-form.error name="category" />
-                </x-form.field>
-                <x-form.button>Publicar</x-form.button>
-            </form>
-        </x-panel>
-    </section>
+            <x-form.field>
+                <x-form.label name="category" />
+                <select name="category_id" id="category">
+                    @foreach (\App\Models\Category::all() as $category)
+                        <option value="{{ $category->id }}"
+                            {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                            {{ ucwords($category->name) }}</option>
+                    @endforeach
+                </select>
+                <x-form.error name="category" />
+            </x-form.field>
+            <x-form.button>Publicar</x-form.button>
+        </form>
+    </x-setting>
 </x-layout>
