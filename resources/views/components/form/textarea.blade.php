36c5c80fc0de7c4bf60874ed655f0f0f0d251f61
Carlos Salvatierra <cslucano@gmail.com>
2023-02-01 11:56:27 -0500

    episode 67

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,7 +1,10 @@
 @props(['name'])
 <x-form.field>
     <x-form.label name="{{ $name }}" />
-    <textarea class="border border-gray-200 p-2 w-full rounded" name="{{ $name }}" id="{{ $name }}"
-        rows="5" placeholder="" required>{{ old($name) }}</textarea>
+    <textarea 
+        class="border border-gray-200 p-2 w-full rounded" 
+        name="{{ $name }}" 
+        id="{{ $name }}"
+        rows="5" placeholder="" required>{{ $slot ?? old($name) }}</textarea>
     <x-form.error name="{{ $name }}" />
 </x-form.field>
